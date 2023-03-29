d42618aa0895b44c37e26994b4df95f48be9328a
chrisurline <chris.surline@gmail.com>
2023-03-28 20:16:22 -0400

    update

--- data/open_source_feed.py
+++ data/open_source_feed.py
@@ -5,10 +5,24 @@ class VirusTotal:
         self.api_key  = api_key
         self.base_url = 'https://www.virustotal.com/api/v3/'
 
-    def get_ioc_data(self, ioc):
+    def get_ioc_data(self, ioc, ioc_type):
+        match ioc_type:
+            case 'ipv4' | 'ipv6':
+                ioc_type = 'ip_addresses'
+            case 'url':
+                ioc_type = 'urls'
+            case 'domain':
+                ioc_type = 'domains'
+            case 'md5' | 'sha1' | 'sha256' | 'sha512':
+                ioc_type = 'hashes'
 
-
-        pass
+        url = f'{self.base_url}{ioc_type}{ioc}'
+        headers = {
+            "accept": "application/json",
+            "x-apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        return response.text
 
 class  AlientVaultOTX:
     def __init__(self, api_key):
@@ -27,8 +41,8 @@ def collect_data(inputs):
 
 
     # run the IOC query
-    vt_ioc_data = vt.get_ioc_data(inputs['ioc'])
-    otx_ioc_data =  vt.get_ioc_data(inputs['ioc'])
+    vt_ioc_data = vt.get_ioc_data(inputs['ioc'], inputs['ioc_type'])
+    otx_ioc_data =  otx.get_ioc_data(inputs['ioc'])
 
     open_source_intel = {
         "virustotal": vt_ioc_data,
