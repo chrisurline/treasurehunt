6e7a78861f2d53542dfc2bfaf1b6b222945c4784
chrisurline <chris.surline@gmail.com>
2023-03-28 23:58:21 -0400

    update + readme

--- analysis/threat_analysis.py
+++ analysis/threat_analysis.py
@@ -1,14 +1,6 @@
 
 def consolidate_intelligence(open_source_data, social_media_data, dark_web_data, malware_sample_data):
-    """
-    Consolidate threat intelligence from various data sources into a single structure.
-
-    :param open_source_data: Collected data from open-source feeds
-    :param social_media_data: Collected data from social media and forums
-    :param dark_web_data: Collected data from dark web sources
-    :param malware_sample_data: Collected data from malware sample repositories
-    :return: A dictionary containing the consolidated threat intelligence
-    """
+   
     consolidated_intel = {
         "open_source_feeds": open_source_data,
         "social_media": social_media_data,
